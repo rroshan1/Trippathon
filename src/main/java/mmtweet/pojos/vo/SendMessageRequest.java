21818713d0b848b03443414e024beb9bead32861
Geetika Gupta <Geetika.Gupta@makemytrip.com>
2016-08-26 18:03:33 +0530

    dal layer

--- src/main/java/mmtweet/pojos/vo/SendMessageRequest.java
+++ src/main/java/mmtweet/pojos/vo/SendMessageRequest.java
@@ -6,7 +6,17 @@ public class SendMessageRequest {
 
 	private String userId;
 	private TweetMessage message;
-	
-	
-	
+
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public TweetMessage getMessage() {
+		return message;
+	}
+	public void setMessage(TweetMessage message) {
+		this.message = message;
+	}	
 }
--- src/main/java/mmtweet/webservices/MMTweetController.java
+++ src/main/java/mmtweet/webservices/MMTweetController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import mmtweet.dal.IMmtweetDal;
 import mmtweet.pojos.vo.BaseResponse;
 import mmtweet.pojos.vo.DoCommentRequest;
 import mmtweet.pojos.vo.GetLiveMessagesRequest;
@@ -18,28 +19,51 @@ import mmtweet.pojos.vo.SendMessageRequest;
 
 @Controller
 public class MMTweetController {
+	
+	IMmtweetDal dal;
+	
 	@RequestMapping(value = "/sendMessage", method = RequestMethod.POST)
 	@ResponseBody
 	public BaseResponse sendMessage(@RequestBody SendMessageRequest request){
-		return new BaseResponse(true);
+		boolean status = false;
+		if (request != null)
+		{
+			status = dal.addMessage(request.getUserId(), request.getMessage());
+		}
+		return new BaseResponse(status);
 	}
 
 	@RequestMapping(value = "/doComment", method = RequestMethod.POST)
 	@ResponseBody
 	public BaseResponse doComment(@RequestBody DoCommentRequest request){
-		return new BaseResponse(true);
+		boolean status = false;
+		if (request != null)
+		{
+			status = dal.addComment(request.getUserId(), request.getMessageId(), request.getComment());
+		}
+		return new BaseResponse(status);
 	}
 
 	@RequestMapping(value = "/getMyMessages", method = RequestMethod.GET)
 	@ResponseBody
 	public GetMyMessagesResponse getMyMessages(@RequestBody GetMyMessagesRequest request){
-		return new GetMyMessagesResponse();
+		GetMyMessagesResponse response = new GetMyMessagesResponse();
+		if (request != null)
+		{
+			response.setMessageList(dal.getMessagesByUserId(request.getUserId()));
+		}
+		return response;
 	}
 
 	@RequestMapping(value = "/getMyComments", method = RequestMethod.GET)
 	@ResponseBody
 	public GetMyCommentsResponse getMyComments(@RequestBody GetMyCommentsRequest request){
-		return new GetMyCommentsResponse();
+		GetMyCommentsResponse response = new GetMyCommentsResponse();
+		if (request != null)
+		{
+			response.setMessageList(dal.getCommentedMessagesByUserId(request.getUserId()));
+		}
+		return response;
 	}
 
 	@RequestMapping(value = "/getLiveMessages", method = RequestMethod.GET)
